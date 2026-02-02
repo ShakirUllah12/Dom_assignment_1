4f89220e77b9f994b72395853075eabeba77a15c
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-02-02 17:32:13 +0500

    dom assignment 1

---
+++ script.js
@@ -0,0 +1,28 @@
+// Task 1
+// let heading=document.getElementsByTagName("h1");
+// heading[0].innerText="well come to dom";
+
+// Task 2
+
+// function change(){
+//     let para=document.getElementsByTagName("p");
+//     para[0].innerText="paragraphtext change";
+// }
+
+// Task 3
+// function change(){
+//     document.body.style.backgroundColor="lightBlue"
+// }
+
+// Task 4
+
+// function show(){
+//     alert("dom is working")
+// }
+
+// Task 5
+function updateText(){
+    let value=document.getElementById("inputText").value;
+    let paragrap=document.getElementById("para")
+    paragrap.innerText=value;
+}
