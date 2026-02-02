83ce93ed2daf148a3f92e5166ae06dab5b9733c2
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-02-02 18:15:33 +0500

    javascrip dom second Assignment

---
+++ Assignment_2/script.js
@@ -0,0 +1,22 @@
+//  Task 1
+//  let heading=document.getElementById("wellcome");
+// heading.innerText="dom element select by id";
+// Task 2
+// let para=document.getElementsByTagName("p");
+// para[2].innerText="First paragraph updated"
+
+// task 3
+// let buttons=document.getElementsByClassName("btn");
+// buttons[1].innerText="click me";
+
+// Task 4
+// let heading=document.querySelector("#heading");
+// heading.style.color="blue"
+// let unorder=document.querySelectorAll("ul");
+// unorder[0].style.color="green";
+
+// Task 5
+let para=document.querySelector(".para");
+para.style.fontSize="100px"
+para.style.color="red"
+para.style.backgroundColor="green"
