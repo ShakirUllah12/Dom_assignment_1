3b3ed2b93bf75b5cba38863d0d7a2bbe59ea50ef
ShakirUllah12 <shakirkhangadooni@gmail.com>
2026-02-02 22:57:19 +0500

    javascrip dom 3rd assignment

---
+++ Assignment_3/script.js
@@ -0,0 +1,41 @@
+// Task 1
+// let image = document.querySelector("#myImage");
+
+// let srcValue = image.getAttribute("src");
+
+// console.log("The src value of the image is:", srcValue);
+
+// Task 2
+
+// let image=document.querySelector("#myImage")
+// let button=document.querySelector("changeBtn");
+// button.addEventListener("click",function(){
+//     image.src="../abbr.png";
+// })
+
+// Task 3
+// let inputs=document.getElementById("input");
+// inputs.setAttribute("placeholder","enter your name")
+// Task 4
+
+// function enableInput(){
+//     let input=document.getElementById("muInput")
+//     input.removeAttribute("disabled")
+// }
+
+// Task 5
+
+// let paragraph = document.getElementById("para");
+
+// let hasClass = paragraph.hasAttribute("class");
+// console.log(hasClass);
+
+// Task 6
+
+// function changeLink(){
+//     let link=document.getElementById("myLink")
+//     link.setAttribute("href","https://www.youtube.com/")
+// }
+
+
+
